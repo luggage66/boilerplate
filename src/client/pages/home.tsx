3d6eb827222858212f8bb3de58e985715b20d29a
Donald L Mull Jr <donald.mull@gmail.com>
2017-07-22 16:43:00 -0400

    Toying with style

--- src/client/pages/home.tsx
+++ src/client/pages/home.tsx
@@ -1,19 +1,25 @@
 import * as React from 'react';
 import { Link } from '../routing';
+import styles from '../styles';
 import { IPageComponentProps } from '../routing';
 
 export default class HomePage extends React.Component<IPageComponentProps, void>
 {
     render() {
         return <div>
-            <ul>
-                <li>
-                    <Link route="viewUser" params={{ id: 111 }}>User #111</Link>
-                </li>
-                <li>
-                    <Link route="viewUser" params={{ id: 333 }}>User #333</Link>
-                </li>
-            </ul>
+            <div className={styles.box}>
+                <ul>
+                    <li>
+                        <Link route="viewUser" params={{ id: 111 }}>User #111</Link>
+                    </li>
+                    <li>
+                        <Link route="viewUser" params={{ id: 333 }}>User #333</Link>
+                    </li>
+                </ul>
+                <button className={styles.button}>
+                    Test
+                </button>
+            </div>
         </div>;
     }
 }
